ce2c875f51d2edb4cab8e87eb471081d4838aeec
Chocolatey <chocolatey@realdimensions.net>
2017-06-23 16:27:43 +0000

    AU: 1 updated - datadog-agent [skip ci] https://gist.github.com/4c97dd2fb3837d302ee0fbd5750e7f74/9b868c0c697c7fb399f17ede1239606335fb49fc

--- datadog-agent/tools/chocolateyinstall.ps1
+++ datadog-agent/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$ErrorActionPreference = 'Stop'; # stop on all errors
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 
 $packageName= 'datadog-agent'
 
@@ -6,8 +6,8 @@ $packageArgs = @{
   packageName   = $packageName
   softwareName  = 'Datadog Agent'
   fileType      = 'MSI'
-  url64bit      = 'https://s3.amazonaws.com/ddagent-windows-stable/ddagent-cli-5.13.0.msi'
-  checksum64    = '0afe5730741250dc05cb298d24efc4d33783c3c6ea4a1b853445f87c40e3c'
+  url64bit      = 'https://s3.amazonaws.com/ddagent-windows-stable/ddagent-cli-5.14.1.msi'
+  checksum64    = '0AFE5730741250DC05CB298D24EFC4D33783C3C6EA4A1B853445F87C40E3C79B'
   checksumType64= 'sha256'
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
